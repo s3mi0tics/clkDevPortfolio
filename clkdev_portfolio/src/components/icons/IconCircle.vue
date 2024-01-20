b78e2badd66598ad54003d4500a47bd2032b4d9b
s3miOtics <colby.kauk@gmail.com>
2024-01-19 20:51:56 -0800

    make icon components using bootstrap icons

---
+++ clkdev_portfolio/src/components/icons/IconCircle.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div>
+        <i class="bi bi-circle-fill"></i>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.bi {
+    height: 44px;
+}
+</style>
