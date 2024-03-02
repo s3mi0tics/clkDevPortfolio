4c5d766a1248d5b38b663235014a51efc2c17402
s3miOtics <colby.kauk@gmail.com>
2024-03-02 09:39:40 -0800

    add admin dashboard project to portfolio

--- src/components/icons/IconCircle.vue
+++
@@ -1,5 +0,0 @@
-<template>
-        <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="currentColor" class="bi bi-circle-fill" viewBox="0 0 16 16">
-                <circle cx="8" cy="8" r="8"/>
-        </svg>
-</template>
